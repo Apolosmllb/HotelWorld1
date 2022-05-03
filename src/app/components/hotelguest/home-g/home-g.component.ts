ceeb2f7feca491acf43b1f893d241b61269a0042
Moisesmp75 <48933728+Moisesmp75@users.noreply.github.com>
2022-05-03 18:52:05 -0500

    Update home-g.component.ts

--- src/app/components/hotelguest/home-g/home-g.component.ts
+++ src/app/components/hotelguest/home-g/home-g.component.ts
@@ -1,1 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-g',
+  templateUrl: './home-g.component.html',
+  styleUrls: ['./home-g.component.css']
+})
+export class HomeGComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
 
