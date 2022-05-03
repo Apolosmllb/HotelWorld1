7636f2b8a011334c44f4027d270c21413b268345
Moisesmp75 <48933728+Moisesmp75@users.noreply.github.com>
2022-05-03 18:53:36 -0500

    Update navbar-g.component.ts

--- src/app/components/hotelguest/navbar-g/navbar-g.component.ts
+++ src/app/components/hotelguest/navbar-g/navbar-g.component.ts
@@ -1,15 +1,1 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-navbar-g',
-  templateUrl: './navbar-g.component.html',
-  styleUrls: ['./navbar-g.component.css']
-})
-export class NavbarGComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
